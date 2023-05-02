6ad393d3e82a5c65e4811bfc62551c1669f36422
dmahfouz <david.mahfouz@gmail.com>
2023-05-02 11:25:07 +0100

    added animations and links to recipe/ingredients

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,16 @@
 import Veggie from "../components/Veggie";
 import Popular from "../components/Popular";
+import { motion } from 'framer-motion';
+
 
 import React from 'react'
 
 function Home() {
     return (
-        <div>
+        <motion.div>
             <Veggie />
             <Popular />
-        </div>
+        </motion.div>
     );
 }
 
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -2,16 +2,21 @@ import React from 'react'
 import Home from './Home'
 import Cuisine from './Cuisine';
 import Seached from './Seached';
-import { Route, Routes } from 'react-router-dom';
-
+import Recipe from './Recipe';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 
 function Pages() {
+    const location = useLocation();
     return (
-        <Routes>
+        <AnimatePresence >
+            <Routes location={location} key={location.pathname}>
             <Route path="/" element={<Home />} />
             <Route path="/cuisine/:type" element={<Cuisine />} />
             <Route path="/searched/:search" element={<Seached />} />
+                <Route path="/recipe/:name" element={<Recipe />} />
         </Routes>
+        </AnimatePresence>
     );
 }
 
